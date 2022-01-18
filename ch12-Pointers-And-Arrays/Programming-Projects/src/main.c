65c21031aa00399b2368a3c2881ea12ce1df02c6
Patrick Mims <patrickkevinmims@gmail.com>
2022-01-17 22:21:02 -0800

    project init

---
+++ ch12-Pointers-And-Arrays/Programming-Projects/src/main.c
@@ -0,0 +1,18 @@
+/* Write a program that reads a message, then prints the reveral of the message:
+ *
+ * Example:
+ * Enter a message: Don't get mad, get even.
+ * Reversal is: .neve teg , dam teg t'noD
+ *
+ * Hint: Read the message one character at a time (using getchar) and store the
+ * the characters in an array. Stop reading when the array is full or the character
+ * read is '\n'.
+ *
+ * b) Revise the program to use a pointer instead of an integer to keep track of the
+ * current position in the array.
+ * */
+
+int main(int argc, char **argv)
+{
+  return 0;
+}
