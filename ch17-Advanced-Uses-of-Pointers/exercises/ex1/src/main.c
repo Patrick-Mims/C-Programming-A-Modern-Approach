53a93a2ef740a81709c1724d0a67209eb5b1d072
Patrick Mims <patrickkevinmims@gmail.com>
2022-01-18 13:18:45 -0800

    latest

--- ch17-Advanced-Uses-of-Pointers/exercises/ex1/src/main.c
+++ ch17-Advanced-Uses-of-Pointers/exercises/ex1/src/main.c
@@ -18,23 +18,36 @@
 
 struct node
 {
-  void *item;
+  char *item;
 };
 
 struct node *my_malloc(struct node *list)
 {
-  if((list = malloc(sizeof(list))) == NULL)
+  if((list = malloc(sizeof(struct node))) == NULL)
   {
     return NULL;
   }
   return list;
 }
 
+struct node *insert(struct node *list, char *str)
+{
+  struct node *new_node = NULL;
+
+  //new_node = my_malloc();
+
+  strcpy(new_node->item, str)
+}
+
 int main(int argc, char **argv)
 {
   struct node *head = NULL;
 
-  my_malloc(head);
+  head = my_malloc(head);
+
+  head->item = "hello";
+
+  printf("%s\n", head->item);
 
   return 0;
 }
