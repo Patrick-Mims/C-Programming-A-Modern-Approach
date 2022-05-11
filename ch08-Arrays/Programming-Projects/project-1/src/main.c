9e2ec9621290a41c8a55834f4d224f10a5847ed6
Patrick Mims <patrickkevinmims@gmail.com>
2022-05-11 15:40:55 -0700

    latest

--- ch08-Arrays/Programming-Projects/project-1/src/main.c
+++ ch08-Arrays/Programming-Projects/project-1/src/main.c
@@ -1,11 +1,61 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-#define TRUE  __bool_true_false_are_defined
+#define SIZE_10 10
+
+void fileIO()
+{
+  printf("Inside fileIO\n");
+  char line[16] = {0};
+  FILE *file;
+  unsigned int line_count = 0;
+
+  if((file = fopen("MOCK_DATA.csv", "r")) == NULL)
+    exit(EXIT_FAILURE);
+
+  while(fgets(line, 16, file))
+  {
+    printf("line[%06d]: %s", ++line_count, line);
+  }
+
+  fclose(file);
+}
 
 int main(int argc, char **argv)
 {
-  printf("true or false -> %d\n", TRUE);
+  bool digit_seen[SIZE_10] = {false};
+
+  int digit = 0;
+  long number = 0;
+
+  printf("Enter A Number: ");
+
+  scanf("%ld", &number);
+
+  printf("Number Entered: %ld\n", number);
+
+  while(number > 0)
+  {
+    digit = number % 10;
+
+    if(digit_seen[digit])
+      break;
+
+    digit_seen[digit] = true;
+
+    number /= 10;
+  }
+
+  if (number > 0)
+  {
+    printf("Repeated digit\n");
+  } else {
+    printf("No Repeated digit\n");
+  }
+
+  fileIO();
 
   return 0;
 }
+/* Checks numbers for repeated digits */
