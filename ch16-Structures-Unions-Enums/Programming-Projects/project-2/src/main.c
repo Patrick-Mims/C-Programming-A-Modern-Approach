c01d46aa7599e2eba5c18c152035368d50482cf5
Patrick Mims <patrickkevinmims@gmail.com>
2022-05-25 13:49:11 -0700

    latest

--- ch16-Structures-Unions-Enums/Programming-Projects/project-2/src/main.c
+++ ch16-Structures-Unions-Enums/Programming-Projects/project-2/src/main.c
@@ -2,10 +2,63 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "inventory.h"
+void insert()
+{
+    printf("insert()\n");
+}
+
+void print()
+{
+    printf("print()\n");
+}
+
+void search()
+{
+    printf("search()\n");
+}
+
+void update()
+{
+    printf("update()\n");
+}
+
+void quit()
+{
+    printf("quit()\n");
+}
+
+void init_start(void)
+{
+  char code;
+  int cnt = 0;
+
+  do
+  {
+    printf("Operation Code [i-nsert, p-rint, s-earch, u-pdate, q-uit]: ");
+
+    while((code = getchar()) != '\n')
+    {
+      printf("-> %c\n", code);
+      switch(code)
+      {
+        case 'i':insert();
+                 break;
+        case 'p':print();
+                 break;
+        case 's':search();
+                 break;
+        case 'u':update();
+                 break;
+        case 'q':quit();
+                 break;
+      }
+    }
+    cnt = cnt + 1;
+  } while(cnt < 5);
+}
 
 int main(int argc, char **argv)
 {
-
+  init_start();
   return 0;
 }
