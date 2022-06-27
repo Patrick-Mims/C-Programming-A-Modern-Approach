3758a90e9c9c515378485ba4d3d674278332878f
Patrick Mims <patrickkevinmims@gmail.com>
2022-06-27 11:10:20 -0700

    Implentation of a Stack, with incomplete type

---
+++ ch19-Program-Design/exercises/01-stack/include/stack.h
@@ -0,0 +1,23 @@
+#ifndef STACK_H
+#define STACK_H
+
+#define INDEX 20
+
+typedef int type;
+
+struct node
+{
+  type data;
+  struct node *nextPtr;
+};
+
+typedef struct stack_type *Stack;
+
+void init();
+bool is_empty();
+int is_full();
+int push(int);
+int pop();
+void make_empty();
+
+#endif
---
+++ ch19-Program-Design/exercises/01-stack/src/main.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "stack.h"
+
+int main(void)
+{
+  init();
+  is_empty();
+
+  return 0;
+}
