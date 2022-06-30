2477101f474b60f92661e3bd7a3b914a666eb090
Patrick Mims <patrickkevinmims@gmail.com>
2022-06-30 08:27:27 -0700

    latest

--- ch19-Program-Design/exercises/01-stack/src/stackImpl.c
+++ ch19-Program-Design/exercises/01-stack/src/stackImpl.c
@@ -1,18 +1,16 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #include "stack.h"
 
-#define INDEX 50
+#define STACK_SIZE 50
 
 struct stack_t
 {
-  int contents[INDEX];
+  int contents[STACK_SIZE];
   int top;
 };
-
-//struct node *head = NULL;
 
 Stack create_stack()
 {
@@ -26,85 +24,36 @@ Stack create_stack()
   return st;
 }
 
-bool is_empty(Stack s)
+Stack destroy_stack(Stack stack)
 {
-  return s->top == 0;
+  free(stack);
 }
 
-int pop(Stack p)
+bool is_empty(Stack stack)
 {
-  printf("p->data\n");
-  if(is_empty(p))
+  return stack->top == 0;
+}
+
+bool is_full(Stack stack)
+{
+  return stack->top == STACK_SIZE;
+}
+
+int pop(Stack stack)
+{
+  if(is_empty(stack))
     exit(EXIT_FAILURE);
 
-  return p->contents[--p->top];
+  return stack->contents[--stack->top];
 }
 
-bool is_full(Stack f)
+void push(Stack stack, int data)
 {
-  return f->top == INDEX;
-}
-
-void push(Stack st, int data)
-{
-  if(is_full(st))
+  if(is_full(stack))
   {
     printf("Stack is Full\n");
     exit(EXIT_FAILURE);
   }
 
-  st->contents[st->top++] = data;
+  stack->contents[stack->top++] = data;
 }
-
-/*
-static void terminate(const char *message)
-{
-  printf("%s\n", message);
-  exit(EXIT_FAILURE);
-}
-
-void make_empty()
-{
-  while(!is_empty(struct node *s))
-  {
-    pop();
-  }
-}
-
-int is_full()
-{
-  return 0;
-}
-
-int push(int item)
-{
-  struct node *newNode = NULL;
-
-  if((newNode = malloc(sizeof(struct node))) == NULL)
-    exit(EXIT_FAILURE);
-
-  newNode->data = item;
-  newNode->nextPtr = top;
-  top = newNode;
-}
-
-int pop()
-{
-  struct node *old_top = NULL;
-
-  if((old_top = malloc(sizeof(struct node))) == NULL)
-    terminate("Error in pop: memmory for old_top cannot be allocated.");
-
-  int i;
-
-  if(is_empty(struct node *s))
-    terminate("Error in pop: stack is empty.");
-
-  old_top = top;
-  i = top->data;
-  top = top->nextPtr;
-  free(old_top);
-
-  return i;
-}
-*/
