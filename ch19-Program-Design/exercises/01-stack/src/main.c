10270addbe2518548b31dd9cd5b7d70b3421abf7
Patrick Mims <patrickkevinmims@gmail.com>
2022-06-30 08:27:14 -0700

    latest

--- ch19-Program-Design/exercises/01-stack/src/main.c
+++ ch19-Program-Design/exercises/01-stack/src/main.c
@@ -6,17 +6,24 @@
 
 int main(void)
 {
-  type result = 0;
-  Stack item = create_stack();
+  Stack stack = create_stack();
 
-  if(is_empty(item) == 0)
+  if(is_empty(stack) == 0)
     printf("\nStack is empty\n");
 
-  push(item, 30);
+  push(stack, 100);
+  push(stack, 200);
+  push(stack, 300);
+  push(stack, 400);
+  push(stack, 500);
 
-  result = pop(item);
+  printf("Item popped off the stack-> %d\n", pop(stack));
+  printf("Item popped off the stack-> %d\n", pop(stack));
+  printf("Item popped off the stack-> %d\n", pop(stack));
+  printf("Item popped off the stack-> %d\n", pop(stack));
+  printf("Item popped off the stack-> %d\n", pop(stack));
 
-  printf("Item popped off the stack-> %d\n", result);
+  destroy_stack(stack);
 
   return 0;
 }
