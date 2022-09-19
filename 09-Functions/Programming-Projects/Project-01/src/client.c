37c9de7a6d83fbb9bbfe134e63a9948afc73fb0f
Patrick <patrickkevinmims@gmail.com>
2022-09-19 16:07:34 -0700

    Latest Changes

--- 09-Functions/Programming-Projects/Project-01/src/client.c
+++ 09-Functions/Programming-Projects/Project-01/src/client.c
@@ -7,6 +7,27 @@ int main(void)
 {
   int digit_seen[10] = {0};
 
+  int digit;
+  long n;
+
+  printf("Enter a number");
+  scanf("%ld", &digit);
+
+  while(n > 0)
+  {
+    digit = n % 10;
+
+    if(digit_seen[digit])
+      break;
+    digit_seen[digit] = true;
+    n /= 10;
+  }
+
+  if(n > 0)
+    printf();
+  else
+    printf("No repeated digit\n");
+
   return 0;
 }
 
